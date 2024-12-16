d2abfe3ffbd1cc3ef755400ba0722db3ecd6fdd6
Chao Zhan <chao.zhan@campus.tu-berlin.de>
2024-12-16 21:53:17 +0100

    use goroutine to speed-up data insertion

--- control/cmd/load.go
+++ control/cmd/load.go
@@ -2,16 +2,27 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+	"sync"
 
+	"context"
 	dg "csb/data-generator"
+	"time"
 
 	"github.com/spf13/cobra"
+	"go.etcd.io/etcd/api/v3/v3rpc/rpctypes"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 var endpoints []string
 var count int
+
+const (
+	dialTimeout    = 5 * time.Second
+	requestTimeout = 10 * time.Second
+)
 
 var LoadCmd = &cobra.Command{
 	Use:   "load [flags] <count>",
@@ -29,7 +40,7 @@ var LoadCmd = &cobra.Command{
 				fmt.Printf("Invalid count: %s\n", args[0])
 			}
 			fmt.Printf("Loading %d records into the database via the following endpoints: %v\n", count, endpoints)
-			load_db(count)
+			load_db(count, endpoints)
 		}
 	},
 }
@@ -38,10 +49,42 @@ func init() {
 	LoadCmd.Flags().StringSliceVar(&endpoints, "endpoints", []string{"127.0.0.1:2379"}, "List of endpoints of the database to load data into")
 }
 
-func load_db(count int) {
+func load_db(count int, endpoints []string) {
 	dataGenerator := dg.NewGenerator(GConfig.rg)
 	data := dataGenerator.GenerateData(count)
-	for k := range data {
-		fmt.Println("Key: ", k)
+
+	dbClient, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: requestTimeout,
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer dbClient.Close()
+
+	var wg sync.WaitGroup
+
+	for key, value := range data {
+		wg.Add(1)
+		go func(key string, value []byte) {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+			defer cancel()
+			_, err := dbClient.Put(ctx, key, string(value))
+			if err != nil {
+				switch err {
+				case context.Canceled:
+					log.Fatalf("ctx is canceled by another routine: %v\n", err)
+				case context.DeadlineExceeded:
+					log.Fatalf("ctx is attached with a deadline is exceeded: %v\n", err)
+				case rpctypes.ErrEmptyKey:
+					log.Fatalf("client-side error: %v\n", err)
+				default:
+					log.Fatalf("bad cluster endpoints, which are not etcd servers: %v\n", err)
+				}
+			}
+		}(key, value)
+	}
+	wg.Wait()
+	fmt.Println("Data loaded successfully")
 }
